c170f73392de694cb9033d7663f8430aed9b1ce6
Ankush <markad.ankush@yahoo.com>
2019-01-03 13:06:15 +0530

    Ascending sort

--- externalarrayjs.js
+++ externalarrayjs.js
@@ -5,6 +5,16 @@ var numArray = [1,2,3,4,5,6,7,8,9,10,12,14];
 var fruits =  ["Apple","Grapes", "Mango","Banana","Pineapple","goava"];
 function arraysort()
 {
-    numsortarray.sort();
+    fruits.sort();
     document.getElementById("sortpid").innerHTML = fruits;
 }
+
+
+var numSortArray =  [1,2,4,5,11,12,22,23,31];
+function ascdescSort()
+{
+    numSortArray.sort(function(num1,num2){return num1 - num2});
+    document.getElementById("ascdescsortpid").innerHTML = numSortArray;
+}
+
+
