f452f72898d0a471ccf7697ba401df34a43d16f2
Ankush <markad.ankush@yahoo.com>
2019-01-03 11:26:35 +0530

    save

--- externalarrayjs.js
+++ externalarrayjs.js
@@ -1,22 +1,21 @@
 
-var numArray = [1,2,3,4,5,6,7,8,9,10,12,14];
 
-function findOddEvenvalues(numArray1)
+function findOddEvenvalues()
 {
+    var numArray = [1,2,3,4,5,6,7,8,9,10,12,14];
     var oddarray1 = [];
     var evenarray1 = [];
-    for (var i=0;i<numArray1.length;i++)
+    for (var i=0;i<numArray.length;i++)
     {
-        if(i % 2 == 0) 
+        if(i % 2 === 0) 
         { 
-            evenarray1.push(a[i]);   
+            evenarray1.push(numArray[i]);   
         }
         else
         {
-            oddarray1.push(a[i]); 
+            oddarray1.push(numArray[i]); 
         }
     }
 
-    document.getElementById("oddevenpid").innerHTML = evenarray1;
-    document.getElementById("oddevenpid").innerHTML = oddarray1;
+    document.getElementById("oddevenpid").innerHTML = "Even Numbers:" + evenarray1 + "<br/>Odd Numbers:" + oddarray1;
 }
