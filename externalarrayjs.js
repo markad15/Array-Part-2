b4032c4dbf7744071541e95e73e289baba9db1f9
Ankush <markad.ankush@yahoo.com>
2019-01-03 12:55:53 +0530

    3 point save

--- externalarrayjs.js
+++ externalarrayjs.js
@@ -5,6 +5,6 @@ var numArray = [1,2,3,4,5,6,7,8,9,10,12,14];
 var fruits =  ["Apple","Grapes", "Mango","Banana","Pineapple","goava"];
 function arraysort()
 {
-    fruits.sort();
+    numsortarray.sort();
     document.getElementById("sortpid").innerHTML = fruits;
 }
