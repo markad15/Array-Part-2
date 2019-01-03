2127d78b5b17a4f41334433d9def9ed23b4f1ce9
Ankush <markad.ankush@yahoo.com>
2019-01-03 12:46:35 +0530

    sort array

--- externalarrayjs.js
+++ externalarrayjs.js
@@ -1,21 +1,10 @@
 
+var numArray = [1,2,3,4,5,6,7,8,9,10,12,14];
 
-function findOddEvenvalues()
+
+var fruits =  ["Apple","Grapes", "Mango","Banana","Pineapple","goava"];
+function arraysort()
 {
-    var numArray = [1,2,3,4,5,6,7,8,9,10,12,14];
-    var oddarray1 = [];
-    var evenarray1 = [];
-    for (var i=0;i<numArray.length;i++)
-    {
-        if(i % 2 === 0) 
-        { 
-            evenarray1.push(numArray[i]);   
-        }
-        else
-        {
-            oddarray1.push(numArray[i]); 
-        }
-    }
-
-    document.getElementById("oddevenpid").innerHTML = "Even Numbers:" + evenarray1 + "<br/>Odd Numbers:" + oddarray1;
+    fruits.sort();
+    document.getElementById("sortpid").innerHTML = fruits;
 }
