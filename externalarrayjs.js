295d2e84ab144da0c39ac82ade169dc1b580a96d
Ankush <markad.ankush@yahoo.com>
2019-01-04 13:41:06 +0530

    Final Save today

--- externalarrayjs.js
+++ externalarrayjs.js
@@ -6,6 +6,7 @@ function funOddEven(total, number1)
     var oddarray1 = [];
     var evenarray1 = [];
     var flagnum = 0;
+
     if (total % 2 == 0 && flagnum == 0)
     {
         evenarray1.push(total);
@@ -24,9 +25,13 @@ function funOddEven(total, number1)
         oddarray1.push(number1);
     }
 
-    document.getElementById("oddevenpid").innerHTML = "Even Numbers:" + evenarray1 + "<br/>Odd Numbers:" + oddarray1;
+    return "Even Numbers:" + evenarray1 + "<br/>Odd Numbers:" + oddarray1;
 }
 
+function callOddEvenFun(item1)
+{
+    document.getElementById("oddevenpid").innerHTML = numArray.reduce(funOddEven);
+}
 
 
 var fruits =  ["Apple","Grapes", "Mango","Banana","Pineapple","goava"];
@@ -83,7 +88,66 @@ function callGreaterNumber()
 function calldatetime()
 {
     var datetime = new Date();
+    var varMonth, varMonth1, varDay, varDay1, varyear;
+
+    varMonth = (datetime.getMonth() + 1);
+    varDay = (datetime.getDate());
+    varYear = (datetime.getFullYear().toString().substr(-2));
+
+  /*  if (varMonth.length == 0)
+    {
+        varMonth1 = "0" + varMonth;
+    }
+    else{
+        varMonth1 = varMonth;
+    }
+
+    if (varDay.length == 1)
+    {
+        varDay1 = "0" + varDay;
+    }
+    else{
+        varDay1 = varDay;
+    }
+    */
+
+    datetime =  varMonth + "/" + varDay +"/"+ varYear;
     document.getElementById("datetimepid").innerHTML = datetime;
+}
+
+function funmaxnumber()
+{
+    
+    var maxValue = 0;
+    numSortArray.forEach(function(element1)
+        {
+            if(maxValue < element1) 
+            maxValue = element1;
+            document.getElementById("maxValuePid").innerHTML = maxValue;
+        }
+    );
+    
+   /* document.getElementById("maxValuePid").innerHTML = Math.max(1,2,4,5,11,12,22,23,31);  */
+}
+
+function funminnumber()
+{
+    var minValue = 9999;
+    numSortArray.forEach(function(element1)
+        {
+            if(minValue > element1) 
+            minValue = element1;
+            document.getElementById("minValuePid").innerHTML = minValue;
+        }
+    );
+    
+  /* document.getElementById("minValuePid").innerHTML = Math.min(1,2,4,5,11,12,22,23,31); */
+}
+
+function funrandomnum()
+{
+    var rndNum = Math.floor((Math.random() * 10));
+    document.getElementById("rndpid").innerHTML = rndNum;
 }
 
 
